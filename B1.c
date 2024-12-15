4c729d3a8a7921d6d72a3cf9aed63ef1349fba64
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-12-15 16:45:41 +0700

    B1.c

--- B1.c
+++ B1.c
@@ -7,5 +7,8 @@ int main () {
     printf("Dia chi bien y la:%lu\n"),(unsigned long)&y;
     printf("Dia chi cua bien y duoc luu tru tai yPtr:%lu\n",(unsigned long)&yPtr);
     
+    printf("Gia tri ma yPtr tham chieu toi la:%d\n",*yPtr);
+    printf("Gia tri cua bien y la:%d\n",y);
+    
     return 0;
 }
